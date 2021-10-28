7f18aac206985b89a10bbf4899b6b74c8a6fd8bd
Mark Stella <mark.stella@dpc.vic.gov.au>
2021-10-28 13:04:11 +1100

    fixed

--- jaffle-shop/snapshots/src/jaffle_shop/orders.sql
+++ jaffle-shop/snapshots/src/jaffle_shop/orders.sql
@@ -5,11 +5,18 @@
       target_schema=target.schema+'_src_jaffle_shop',
       unique_key='order_id',
       strategy='check',
-      check_cols=['user_id', 'order_date','status']
+      check_cols=['customer_id', 'order_date','status']
     )
 }}
 
-select *
+select 
+    order_id,
+    customer_id,
+    order_date,
+    status,
+    p_year,
+    p_month,
+    p_day
   from {{ ref('orders_vw')}}
 
 {% endsnapshot %}
